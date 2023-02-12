ed453ca02e9dd2d186731fb48ee78bf3bfd9af8e
jxramos <jaime.x.ramos@gmail.com>
2023-02-11 19:31:12 -0800

    First commit stubbing the extension for trulia.com

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # real_escrapes
 Browser extension for scraping real estate websites to download data.
+
+## Web Site Support
+The current list of real estate websites that are supported.
+* trulia.com
---
+++ real_estate_sites/trulia.js
@@ -0,0 +1,8 @@
+function processTruliaPropertyPage() {
+    // Processes URLs like
+    // https://www.trulia.com/p/<state>/<city>/<street_addr>-<city>-<state>-<zip>--<listing_id>
+    console.log("processTruliaPropertyPage")
+
+}
+
+processTruliaPropertyPage();
